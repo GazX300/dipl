618b9c477250d2c3759da1a5620c088d31403408
GazX300 <148624389+GazX300@users.noreply.github.com>
2024-02-13 20:30:57 +0200

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,29 @@
+// Функція для зміни активної секції
 function changeSection(sectionNumber) {
+    // Закрити всі секції
     var sections = document.querySelectorAll('.section');
     sections.forEach(function(section) {
         section.classList.remove('active');
     });
 
+    // Відкрити вибрану секцію
     var section = document.getElementById('section' + sectionNumber);
     section.classList.add('active');
+
+    // Приховати другу секцію, якщо відкрита перша
+    if (sectionNumber === 1) {
+        var secondSection = document.getElementById('section2');
+        secondSection.style.display = 'none';
+    } else {
+        var secondSection = document.getElementById('section2');
+        secondSection.style.display = 'block';
+    }
+    // Приховати першу секцію, якщо відкрита друга
+    if (sectionNumber === 2) {
+        var secondSection = document.getElementById('section1');
+        secondSection.style.display = 'none';
+    } else {
+        var secondSection = document.getElementById('section1');
+        secondSection.style.display = 'block';
+    }
 }
