9073860c58d90e3a8e7bc776b883a005a650129b
GazX300 <148624389+GazX300@users.noreply.github.com>
2024-02-26 13:53:34 +0200

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-// Функція для зміни активної секції
 function changeSection(sectionNumber) {
     // Закрити всі секції
     var sections = document.querySelectorAll('.section');
@@ -11,19 +10,54 @@ function changeSection(sectionNumber) {
     section.classList.add('active');
 
     // Приховати другу секцію, якщо відкрита перша
-    if (sectionNumber === 1) {
-        var secondSection = document.getElementById('section2');
+    var secondSection = document.getElementById('section2');
+    if (sectionNumber === 1 || sectionNumber === 3) {
         secondSection.style.display = 'none';
     } else {
-        var secondSection = document.getElementById('section2');
         secondSection.style.display = 'block';
     }
-    // Приховати першу секцію, якщо відкрита друга
-    if (sectionNumber === 2) {
-        var secondSection = document.getElementById('section1');
-        secondSection.style.display = 'none';
+
+    // Приховати першу секцію, якщо відкрита друга або третя
+    var firstSection = document.getElementById('section1');
+    if (sectionNumber === 2 || sectionNumber === 3) {
+        firstSection.style.display = 'none';
     } else {
-        var secondSection = document.getElementById('section1');
-        secondSection.style.display = 'block';
+        firstSection.style.display = 'block';
+    }
+
+    // Приховати третю секцію, якщо відкрита перша або друга
+    var thirdSection = document.getElementById('section3');
+    if (sectionNumber === 1 || sectionNumber === 2) {
+        thirdSection.style.display = 'none';
+    } else {
+        thirdSection.style.display = 'block';
     }
 }
+function backToTop() {
+    var sections = document.querySelectorAll('.section');
+    sections.forEach(function(section) {
+        section.style.display = 'none';
+    });
+}
+function toggleDropdown() {
+    var dropdown = document.getElementById("myDropdown");
+    dropdown.classList.toggle("show");
+}
+
+function toggleDropdownLab() {
+    var dropdown = document.getElementById("myDropdownLab");
+    dropdown.classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
